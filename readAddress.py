491775c2f413374b2669d881fe8f09c7c4e7dc1c
gerald schenk <gschenk@yorku.ca>
2016-11-07 23:25:51 +0100

    adds address entry parsing script from
    git@github.com:gschenk/address-stuff.git
    commit 7f5d01be625c484a2339a7e511438c6a84f4f8fb

---
+++ readAddress.py
@@ -0,0 +1,84 @@
+# This script expects an address handle as an argument
+# this handle corresponds to the file names of the
+# address entries without extension.
+
+from yaml import load, YAMLError
+from sys import argv
+
+# path to the configuration file
+cfgfile = 'address.cfg'
+
+# temporary asgt
+handle = 'test_test'
+
+def get_path_filename(handle):
+    """ cleans path, combines it"""
+    path = yamlPath.strip('/').strip()
+    return path + '/' + handle + yamlExtension
+
+# MAIN
+
+# read the configuration file, wherin paths, data
+# structures and keys of the yaml input are defined.
+with open(cfgfile) as f:
+        code = compile(f.read(), cfgfile, 'exec')
+        exec(code)
+
+
+# get commandline argument
+for idx, arg in enumerate(argv[1:]):
+    if idx == 0:
+        handle = arg
+    if idx == 1:
+        print("Only the first argument is considered. "
+             + "Here I dump the rest. "
+             + "After that I shall exit disgracefully."
+             )
+    if idx >= 1:
+        print(arg, "\n")
+
+# Do exit disgracefully.
+if idx > 0:
+    print("I told you so!")
+    exit(1)
+
+
+# check for and strip extensions
+if yamlExtension in handle:
+    print("Please avoid extensions ("
+          + yamlExtension
+          + ") in the handle. "
+          + "I shall proceed to strip it now."
+         )
+    handle = handle.strip(yamlExtension)
+
+# some chars must not be in a valid handle
+forbidden = ['/','\\','*']
+for c in forbidden:
+    if c in handle:
+        print("This character cannot be in "
+              + "a valid handle: "
+              + c
+              + "\nAlas, how far I came, "
+              + "yet I must perish."
+             )
+        exit(2)
+
+
+# read address entries from yaml
+infile = get_path_filename(handle)
+with open(infile, 'r') as fp:
+    try:
+        entries = load(fp)
+    except YAMLError as exc:
+        print(exc)
+        exit(3)
+
+
+# what follows is only an example, print to stdout
+for key, val in dict.items(entries):
+    if key != 'theAddress':
+        print(key, val)
+
+for key, val in dict.items(entries["theAddress"]):
+    print(key, val)

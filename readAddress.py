bf43364eb7559f6453775fb1c66754fac66b455e
gerald schenk <gschenk@yorku.ca>
2016-11-08 20:45:46 +0100

    adds refactored and cleaned up version of scripts
    commit f43f8883567490522ee5ba0e41e4e846fcbde561
    git@github.com:gschenk/address-stuff.git

--- readAddress.py
+++ readAddress.py
@@ -11,70 +11,79 @@ cfgfile = 'address.cfg'
 # temporary asgt
 handle = 'test_test'
 
+
 def get_path_filename(handle):
     """ cleans path, combines it"""
     path = yamlPath.strip('/').strip()
     return path + '/' + handle + yamlExtension
 
+
 def eprint(argument):
     """ print to stderr """
     sys.stderr.write(argument)
+
+
+def strip_extension(handle):
+    """ remove extension if one is there """
+    ext = config['extension']
+    if ext in handle:
+        eprint("Please avoid extensions ("
+               + ext
+               + ") in the handle. "
+               + "I shall proceed to strip it now.")
+        return handle.strip(ext)
+    else:
+        return handle
+
+
+def check_if_valid(s):
+    """ warn and return false if handle is invalid """
+    forbidden = r"@%?\/*"
+    for c in s:
+        if c in forbidden:
+            eprint("The characters (" + forbidden
+                   + ") cannot be in a valid string: "
+                   + c + " in " + s
+                   + "\nAlas, how far I came, "
+                   + "yet I must perish.")
+            return False
+    return True
+
 
 # MAIN
 
 # read the configuration file, wherin paths, data
 # structures and keys of the yaml input are defined.
+# the configuration file is also in yaml format
 with open(cfgfile) as f:
-        code = compile(f.read(), cfgfile, 'exec')
-        exec(code)
+        config = load(f)
 
+yamlExtension = config['extension']
+yamlPath = config['path']
 
-# get commandline argument
-for idx, arg in enumerate(sys.argv[1:]):
-    if idx == 0:
-        handle = arg
-    if idx == 1:
-        eprint("Only the first argument is considered. "
-             + "Here I dump the rest. "
-             + "After that I shall exit disgracefully."
-             )
-    if idx >= 1:
-        eprint(arg + "\n")
-
-# Do exit disgracefully.
-if 'idx' not in locals():
+try:
+    handle = (sys.argv[1])
+except:
     eprint("An argument is required, "
-          + "it should specify the handle of "
-          + " the address entry.")
-    eprint("Oh dear, is this the end? Yes, indeed.")
-    exit(5)
+           + "it should specify the handle of "
+           + " the address entry.")
+    eprint("\nOh dear, is this the end? Yes, indeed.")
+    exit(4)
 
-if idx > 0:
-    eprint("I told you so!")
-    exit(1)
+if len(sys.argv) > 2:
+    eprint("Only the first argument is considered. "
+           + "Here I dump the rest:"
+           + str(sys.argv[2:])
+           + "\nAfter that I shall exit disgracefully.")
+    exit(2)
 
 
 # check for and strip extensions
-if yamlExtension in handle:
-    eprint("Please avoid extensions ("
-          + yamlExtension
-          + ") in the handle. "
-          + "I shall proceed to strip it now."
-         )
-    handle = handle.strip(yamlExtension)
+handle = strip_extension(handle)
 
 # some chars must not be in a valid handle
-forbidden = ['/','\\','*']
-for c in forbidden:
-    if c in handle:
-        eprint("This character cannot be in "
-              + "a valid handle: "
-              + c
-              + "\nAlas, how far I came, "
-              + "yet I must perish."
-             )
-        exit(2)
-
+if not check_if_valid(handle):
+    exit(2)
 
 # read address entries from yaml
 infile = get_path_filename(handle)
